f9957f0808b7d54af3d633f33ece4548f4074890
Daniel Bruno <daniel.c.bruno@gmail.com>
2021-09-13 16:18:04 -0400

    Kinnesis draft 1

--- modules/terraform_backend/main.tf
+++ modules/terraform_backend/main.tf
@@ -2,3 +2,18 @@ resource "aws_s3_bucket" "terraform_state" {
     bucket = var.s3_bucket_name
     acl    = "private"
 }
+
+resource "aws_kinesis_stream" "kinesis_stream" {
+  name = "kinesis-stream"
+  shard_count = 1
+  retention_period = 24
+  shard_level_metrics = [
+    "IncomingBytes",
+    "OutgoingBytes"
+  ]
+
+  tags = {
+    Terraform = True
+    Environment = "test"
+  }
+}
